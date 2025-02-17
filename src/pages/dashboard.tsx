030217e89bf721369f7b7b28534f9c13111b0960
Äniebietabasi <aniebietabasiobo@gmail.com>
2025-02-17 08:07:40 +0100

    cmmit

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,11 +1,13 @@
 // import React from 'react'
 import DashComp from '../components/Dashboard'
+import UpcomingIdo from '../components/Launchpad/UpcomingIdo'
 import Layout from '../layout'
 
 function Dashboard() {
   return (
     <Layout>
       <DashComp/>
+      <UpcomingIdo/>
     </Layout>
   )
 }
