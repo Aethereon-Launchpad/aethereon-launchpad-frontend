10ca4af4574293f96cd0063e81d63babbbd25ec4
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-03-04 16:36:33 +0100

    feat: Add social media icons and update footer content

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -57,16 +57,32 @@ function Footer() {
       link: "https://linkedin.com/in/derhex",
     },
   ];
+
+  const footerSocials = [
+    {
+      url: "https://linkedIn.com",
+      img: "/footer/socials/linkedin.svg"
+    },
+    {
+      url: "https://twitter.com",
+      img: "/footer/socials/twitter.svg"
+    },
+    {
+      url: "https://instagram.com",
+      img: "/footer/socials/ig.svg"
+    },
+    {
+      url: "https://facebook.com",
+      img: "/footer/socials/facebook.svg"
+    }
+  ]
   return (
     <div className="flex flex-col w-full font-space p-[20px] lg:p-[40px]">
       <div className="bg-[#000508] text-white lg:p-[40px] grid lg:grid-cols-2 gap-[40px]">
         <div>
           <img src="/derhex-logo.svg" className="h-[26px] lg:h-[38px]" alt="" />
           <p className="w-full lg:w-[50%] text-[14px] lg:text-[16px] mt-[15px] lg:mt-[25px]">
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Placeat,
-            doloremque beatae. Dolorem rerum blanditiis qui quisquam saepe sed,
-            neque asperiores eius tempora dolores suscipit quo possimus earum
-            expedita sequi ducimus?
+            Derhex Pad is a leading decentralized fundraising platform on @soniclabs, incubating the next generation of web3 startups, fueled by $D & $S
           </p>
         </div>
         <div className="grid gap-[30px] lg:gap-0 lg:grid-cols-3">
@@ -104,8 +120,15 @@ function Footer() {
       </div>
       <div className="border-t border-t-[#797979] mt-[20px] pt-[20px]">
         <div className="flex flex-col lg:flex-row items-start lg:items-start justify-between text-white">
-          <div>loll</div>
-          <div>loll</div>
+          <div>Copyright derhex {new Date().getFullYear()}. All Rights Reserved.</div>
+          <div className="flex gap-x-8">
+            {footerSocials.map((sm: { url: string, img: string }, index: number) => {
+              return (
+                <Link to={sm.url} key={index}>
+                  <img src={sm.img} alt="" />
+                </Link>
+              )
+            })}</div>
         </div>
       </div>
     </div>
