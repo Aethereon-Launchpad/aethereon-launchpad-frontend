0baf9774680a8eab1b05320c1ba7c04a02ade441
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-03-07 22:50:56 +0100

    feat: layered work

--- src/pages/ido/index.tsx
+++ src/pages/ido/index.tsx
@@ -1,3 +1,18 @@
-export default function IDO(){
-    
+import IDOInfo from "../../components/IDO/IDOInfo"
+import IDOStats from "../../components/IDO/IDOStats"
+import Layout from "../../layout"
+
+export default function IDO() {
+    return (
+        <Layout>
+            <div className="w-full grid md:grid-cols-6 grid-cols-1 min-h-screen gap-3 my-20 xl:p-[20px_40px]">
+                <div className="col-span-4 md:order-1 order-2">
+                    <IDOInfo />
+                </div>
+                <div className="md:col-span-2 md:order-2 order-1 col-span-4">
+                    <IDOStats />
+                </div>
+            </div>
+        </Layout>
+    )
 }
