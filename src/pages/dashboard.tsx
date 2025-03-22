854a2a1ff416349971981e4d2deed8a33027a4a8
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-03-22 08:13:22 +0100

    feat: Enhance Explore Component with Search Functionality and Improved UI
    - Implemented a search feature to filter presales by project name or contract address.
    - Added loading and error handling states to improve user experience during data fetching.
    - Updated UI elements for better presentation, including a new search input and results display.
    - Enhanced page title for better context when exploring IDOs.

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -4,8 +4,10 @@ import JoinedPool from '../components/Dashboard/JoinedPool'
 import Reward from '../components/Dashboard/Reward'
 import FeaturedIdo from '../components/Home/FeaturedIdo'
 import Layout from '../layout'
+import { usePageTitle } from '../hooks/utils'
 
 function Dashboard() {
+  usePageTitle("Your Dashboard")
   return (
     <Layout>
       <DashComp />
