ea05a47d9c00e91e20bbd8945b903675c83ca84e
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-04-08 20:08:46 +0100

    fix: Enhance typography in SupportedBy and Trusted components for better emphasis
    - Updated the heading in SupportedBy to use a larger font size and extrabold weight for improved visibility.
    - Changed the paragraph in Trusted to a larger font size and extrabold weight to enhance readability and visual hierarchy.

--- src/components/Launchpad/Trusted.tsx
+++ src/components/Launchpad/Trusted.tsx
@@ -6,7 +6,7 @@ function Trusted() {
   return (
     <div>
       <div className="flex font-space flex-col items-center justify-center py-[40px]">
-        <p className="uppercase text-primary text-center font-bold">
+        <p className="uppercase text-2xl text-primary text-center font-extrabold">
           Trusted by blockchain innovators
         </p>
         <div className="flex flex-col lg:flex-row items-center space-y-[30px] lg:space-y-0 lg:space-x-[50px] mt-[20px]">
