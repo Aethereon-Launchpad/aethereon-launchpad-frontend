54c14a6f4e2944aac870991c3bf563f42d7f109b
Adefeyitimi Adeyeloja <33554538+TimmyIsANerd@users.noreply.github.com>
2025-04-14 13:14:41 +0100

    chore: Comment out Community component in Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ function Home() {
       </div>
       <StakingCTA />
       <LaunchProject />
-      <Community />
+      {/* <Community /> */}
       <Access />
       <FAQ />
       <Subscribe />
